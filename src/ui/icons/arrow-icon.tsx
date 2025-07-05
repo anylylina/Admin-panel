160e87baa27fba9a3c398fbe2ed534b9b6f91bf9
Lialina Anna <Lialina2010@ukr.net>
2025-07-05 13:54:04 +0300

    add ui dropdown

---
+++ src/ui/icons/arrow-icon.tsx
@@ -0,0 +1,16 @@
+import type { SVGProps } from 'react';
+
+export const ArrowIcon = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      width="14"
+      height="9"
+      viewBox="0 0 14 9"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="M1 1L7 7L13 1" stroke="#718EBF" strokeWidth="1.5" />
+    </svg>
+  );
+};
